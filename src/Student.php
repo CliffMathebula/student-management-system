ab9b49e92ac7474f59472a1c8056d040381736a7
Cliff Mathebula <climathe1@gmail.com>
2020-02-21 10:20:21 +0200

    feat: Check if student exists

--- src/Student.php
+++ src/Student.php
@@ -5,6 +5,17 @@ namespace ManagementSystem;
 class Student
 {
     /**
+     * Check if student exists
+     *
+     * @param string $id
+     * @return bool
+     */
+    public static function exists(string $id) : bool
+    {
+        return file_exists(self::getPath($id, "{$id}.json"));
+    }
+
+    /**
      * Get student(s) path
      *
      * @param string $id

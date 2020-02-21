7a17219ef7f67ae178061e333bab8e767aed9a07
Cliff Mathebula <climathe1@gmail.com>
2020-02-21 10:17:34 +0200

    feat: Check if value is numeric

--- src/Validation.php
+++ src/Validation.php
@@ -7,5 +7,16 @@ class Validation
     const IsString = 'isString';
 
     const IsNumeric = 'isNumeric';
+
+    /**
+     * Check if value is numeric
+     *
+     * @param string $value
+     * @return bool
+     */
+    public function isString(string $value) : bool
+    {
+        return is_string($value);
+    }
 }
 
